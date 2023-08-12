c19de6240405740ca026ffa2fcbbbcdb5e38e838
professornaite <professornaite@gmail.com>
2023-08-12 15:52:00 -0400

    updates

--- data-raw/africa_data_all_load.R
+++ data-raw/africa_data_all_load.R
@@ -1,1 +1,9 @@
-# africa-data-all_load.R
+# africa_data_all_load.R
+
+install.packages("tidyverse")
+library(tidyverse)
+
+africa_data_all_raw <- readr::read_csv("data-raw/africa_data_all.csv")
+View(africa_data_all_raw)
+
+
