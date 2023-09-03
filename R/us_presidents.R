8880bd9fd7c29cdd8ffdcd01645d60f0aa1b1746
professornaite <professornaite@gmail.com>
2023-09-02 22:56:51 -0400

    data update

---
+++ R/us_presidents.R
@@ -0,0 +1,16 @@
+#' Data on US Presidents through 2023
+#' 
+#' Data contains president, president political party, start data, end data, and vice president by term
+#' 
+#' A tibble with 68 rows and 5 variables
+#' 
+#' \describe{
+#'  \item{potus}{chr President}
+#'  \item{party}{dbl President political party}
+#'  \item{start}{dbl Start data of presidential term}
+#'  \item{end}{dbl End date of presidential term}
+#'  \item{vpotus}{dbl Vice President}
+#'  }
+"us_presidents"
+
+# run devtools::document() in console to make Roxygen comments "concrete"
