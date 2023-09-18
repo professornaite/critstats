f4cd85fbae9a5c152bb770af1459cf863aa93633
professornaite <professornaite@gmail.com>
2023-09-18 12:32:29 -0400

    package updates

--- data-raw/yahoo_data_load.R
+++ data-raw/yahoo_data_load.R
@@ -11,24 +11,8 @@ View(yahoo_data_raw)
 head(yahoo_data_raw, n=10)
 tail(yahoo_data_raw, n=10) # there are a total of 1059 observations in the data set
 
-# generate code to view data set outputs by response variables?
-# what are the three categories and how can we create a simple graph of those?
-# how can we associate those categories to the race of the respondents?
-# generate simple visuals
-  # begin with base r code for factors (bar charts)
-  # continue with intermediate code for factors
-  # end with GGPLOT construction of the variables that you will need to use
-# look at the conditional probabilities of each response by race (see the codebook)
-
 df <- as_tibble(yahoo_data_raw)
 summary(df)
-
-df <- rename(df,
-             "...1" = "respondent.id",
-             "race_eth" = "race",
-             "response" = "equal_treatment"
-             )
-
 
 # remove variable
 df <- select(df, -c(1))
