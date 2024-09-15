dad9d822c2124cd85000b1423818a07d15676c67
professornaite <professornaite@gmail.com>
2024-09-15 16:30:25 -0400

    blackmedian income update

--- data-raw/blackmedianincome_load.R
+++ data-raw/blackmedianincome_load.R
@@ -4,8 +4,8 @@ install.packages("tidyverse")
 library(tidyverse)
 library(dplyr)
 
-census_blackmedianincome_raw <- readr::read_csv("data-raw/census-blackmedianincome.csv")
-df <- as_tibble(census_blackmedianincome_raw) # so the data prints a little nicer
+blackmedianincome_raw <- readr::read_csv("data-raw/blackmedianincome.csv")
+df <- as_tibble(blackmedianincome_raw) # so the data prints a little nicer
 summary(df) # view raw summary statistics
 df
 
